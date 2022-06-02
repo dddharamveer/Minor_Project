af90747eb6922bd043b5220132efe7d8f7b2cec4
Dharamveer <workfordharamveer@gmail.com>
2022-06-02 17:54:38 +0530

    pageanimation

--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,21 @@ import ContentMain from "./components/contentMain";
 import { CSSTransition } from "react-transition-group";
 import Imagetotext from "./components/imagetotext";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Home =(props)=>{
     const [image, setImage] = useState("");
     const imageUrlHandler = (image) => {
       setImage(image);
     };
-return  <div>
+return  <motion.div
+initial={{opacity:0}}
+animate={{opacity:1}}
+exit={{opacity:0}}
+>
 <ContentMain  urlUpload={imageUrlHandler} />
 {/* <Imagetotext image={image} /> */}
-</div>
+</motion.div>
 }
 
 
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,18 +1,56 @@
-import classes from './about.module.css'
-const About =(props)=>{
-    return <div className={classes.about}>
-        <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, ipsam? Quibusdam architecto non veritatis, quod officiis maxime inventore quam. Aliquam vero perspiciatis harum iste blanditiis eveniet atque ullam repellat dolore!
-        Numquam, nobis minima corrupti unde illo tenetur necessitatibus corporis exercitationem, similique sequi, consequatur fuga et magnam consequuntur laudantium perspiciatis omnis repellendus? Blanditiis sed corporis cum ut quos deleniti consequatur. Iure.
-        Natus, placeat quaerat? Aperiam sint maxime recusandae, doloribus et beatae. Voluptatibus, quis fuga quisquam possimus minus, iure totam reprehenderit laboriosam quam ullam dicta rerum, qui voluptatem eum cupiditate exercitationem. Voluptas!
-        Reiciendis mollitia, quis debitis sint laboriosam at maiores aliquam. Fuga, dolorum vero. Libero facere, atque amet laborum necessitatibus, molestiae maiores corrupti aperiam officiis sit quibusdam eaque natus suscipit, autem consequuntur.
-        Maxime reprehenderit id ut labore, pariatur velit ipsa ad tenetur, debitis architecto aperiam quos? Inventore asperiores fuga reiciendis veritatis architecto iusto pariatur quidem, quasi odio exercitationem deserunt sapiente corrupti. In.
-        Commodi doloribus quam sit eligendi beatae nostrum a, dolore id, similique officiis expedita repellendus dolor! Soluta ab, neque ipsum dolore laboriosam ipsam molestiae. Dolorum modi ullam rerum quasi quia magnam.
-        Corrupti temporibus ratione iusto eligendi minima illum similique quis nihil deleniti. Eveniet amet dolorum doloribus magnam facere, adipisci iure vero delectus harum, id odio sunt distinctio ut corporis provident laudantium.
-        Unde, voluptate placeat nesciunt exercitationem non explicabo excepturi fuga perspiciatis? Amet natus nesciunt dignissimos, atque soluta nihil deserunt delectus minima dolor ducimus perferendis quo officiis est, magni vitae, corporis animi?
-        Itaque error placeat, facilis natus quasi voluptatibus vel at dolore quos iure ipsum iusto repudiandae architecto fuga dignissimos ea assumenda eaque veniam eveniet, cum aliquid aperiam temporibus blanditiis soluta! Porro!
-        Impedit laudantium ratione minus architecto aspernatur accusantium dolorem reprehenderit saepe sed similique maiores harum, voluptas quia aperiam voluptate, doloribus nemo aut eligendi quas adipisci ipsa sit. Quasi ullam libero explicabo.</p>
-    </div>
-}
+import classes from "./about.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
+const About = (props) => {
+  return (
+    <AnimatePresence >
+      
+      <motion.div
+        className={classes.about}
+        initial={{ x: -window.innerWidth, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: window.innerWidth, opacity: 0 }}
+      >
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+          ipsam? Quibusdam architecto non veritatis, quod officiis maxime
+          inventore quam. Aliquam vero perspiciatis harum iste blanditiis
+          eveniet atque ullam repellat dolore! Numquam, nobis minima corrupti
+          unde illo tenetur necessitatibus corporis exercitationem, similique
+          sequi, consequatur fuga et magnam consequuntur laudantium perspiciatis
+          omnis repellendus? Blanditiis sed corporis cum ut quos deleniti
+          consequatur. Iure. Natus, placeat quaerat? Aperiam sint maxime
+          recusandae, doloribus et beatae. Voluptatibus, quis fuga quisquam
+          possimus minus, iure totam reprehenderit laboriosam quam ullam dicta
+          rerum, qui voluptatem eum cupiditate exercitationem. Voluptas!
+          Reiciendis mollitia, quis debitis sint laboriosam at maiores aliquam.
+          Fuga, dolorum vero. Libero facere, atque amet laborum necessitatibus,
+          molestiae maiores corrupti aperiam officiis sit quibusdam eaque natus
+          suscipit, autem consequuntur. Maxime reprehenderit id ut labore,
+          pariatur velit ipsa ad tenetur, debitis architecto aperiam quos?
+          Inventore asperiores fuga reiciendis veritatis architecto iusto
+          pariatur quidem, quasi odio exercitationem deserunt sapiente corrupti.
+          In. Commodi doloribus quam sit eligendi beatae nostrum a, dolore id,
+          similique officiis expedita repellendus dolor! Soluta ab, neque ipsum
+          dolore laboriosam ipsam molestiae. Dolorum modi ullam rerum quasi quia
+          magnam. Corrupti temporibus ratione iusto eligendi minima illum
+          similique quis nihil deleniti. Eveniet amet dolorum doloribus magnam
+          facere, adipisci iure vero delectus harum, id odio sunt distinctio ut
+          corporis provident laudantium. Unde, voluptate placeat nesciunt
+          exercitationem non explicabo excepturi fuga perspiciatis? Amet natus
+          nesciunt dignissimos, atque soluta nihil deserunt delectus minima
+          dolor ducimus perferendis quo officiis est, magni vitae, corporis
+          animi? Itaque error placeat, facilis natus quasi voluptatibus vel at
+          dolore quos iure ipsum iusto repudiandae architecto fuga dignissimos
+          ea assumenda eaque veniam eveniet, cum aliquid aperiam temporibus
+          blanditiis soluta! Porro! Impedit laudantium ratione minus architecto
+          aspernatur accusantium dolorem reprehenderit saepe sed similique
+          maiores harum, voluptas quia aperiam voluptate, doloribus nemo aut
+          eligendi quas adipisci ipsa sit. Quasi ullam libero explicabo.
+        </p>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
 
 export default About;
