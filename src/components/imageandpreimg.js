cae62e5b8df5d0735a79ff651cdc63767fc3de14
Dharamveer <workfordharamveer@gmail.com>
2022-06-12 17:08:14 +0530

    1.0

--- src/components/imageandpreimg.js
+++ src/components/imageandpreimg.js
@@ -1,21 +1,56 @@
-import {useContext, useState } from "react";
-
+import { useContext, useState,useRef } from "react";
+import { useDimensions } from "./use-dimensions";
 
 import AuthContext from "../store/auth-context";
-import classes from './imageandpreimg.module.css'
+import classes from "./imageandpreimg.module.css";
+import {motion} from 'framer-motion'
+const ImageModal = (props) => {
+  const ctx = useContext(AuthContext);
+  const sidebar = {
+    
+    open:{
+      clipPath: "circle(100vh at 50% 95%)",
+      transition: {
+        ease: "linear",
+        duration:.5
+      }
+    }
+  ,
+    closed: {
+      clipPath: "circle(5% at 50% 95%)",
+      transition: {
+        ease: "linear",
+        duration:.4
+        
+       
+      }
+    }
+  }
 
 
-const ImageModal=(props)=>{
+  return (
+    <motion.div className={classes.inside}
+    initial={"closed"}
+    animate={ "open"}
+    exit={"closed"}
+    
    
-    const ctx = useContext(AuthContext);
-   
-    return<div className={classes.inside}>
-        <img
-          src={ctx.image && URL.createObjectURL(ctx.image)}
-          className={classes.img}
-        />
- <img src={props.dataUrl} alt="" />
-  </div>
-}
+    variants={sidebar}
+    >
+      <div className={classes.insideDiv}>
+      <div className={classes.mainImage}>
+        <p>Image</p>
+      <img
+        src={ctx.image && URL.createObjectURL(ctx.image)}
+      />
+      </div>
+      <div className={classes.mainImage}>
+        <p>preprocessed image</p>
+      <img src={props.dataUrl} alt="" />
+      </div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default ImageModal;
--- src/components/imagetotext.js
+++ src/components/imagetotext.js
@@ -1,68 +1,61 @@
 import { useContext, useState } from "react";
-import HandleImage from "./image";
-
 import Tesseract from "tesseract.js";
-import classes from "./imagetotext.module.css";
-import Canvas from "./canvas"
 import AuthContext from "../store/auth-context";
-import Button from "./UI/Button";
-import Image from "./image"
-import { motion } from "framer-motion";
-import { Routes, useNavigate,Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import classes from "./imagetotext.module.css";
+
+import Canvas from "./canvas";
 import ImageModal from "./imageandpreimg";
-import NothingFound from "./pages/NothingFound";
+
 const Imagetotext = (props) => {
-  
+  const ctx = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  //Provide data URL
+
   const [dataUrl, setDataUrl] = useState(null);
   const DataUrlHandler = (dataUrl) => {
-      setDataUrl(dataUrl);
-    };
-  
+    setDataUrl(dataUrl);
+  };
+
+  const [first, setfirst] = useState(0);
   const [submit, setSubmit] = useState(false);
   const [status, setstatus] = useState(0);
-  const [inside,setInside] = useState(false);
+  const [inside, setInside] = useState(false);
+
   const SubmitHandler = () => {
     setSubmit((prevState) => !prevState);
   };
-
-  const ctx = useContext(AuthContext);
-  
-  const statusHandler =(e)=>{
+  const statusHandler = (e) => {
     setstatus(e.progress.toFixed(2) * 100);
-    
+  };
+
+  const insideHandler = () => {
+    setInside((prev) => !prev);
+  };
+
+  // Function to run tesseract and get text from it
+  const handleClick = () => {
+    SubmitHandler();
+    Tesseract.recognize(dataUrl, "eng", {
+      logger: (m) => {
+        console.log(m);
+        setstatus(m);
+        setfirst(m.progress);
+      },
+    }).then(({ data: { text } }) => {
+      setfirst(0);
+      ctx.textHandler(text);
+      console.log(text);
+    });
+  };
+
+  //for running the function based to states
+  if (first === 0 && submit) {
+    handleClick();
   }
-const insideHandler = ()=>{
-setInside(prev=>!prev)
-}
-const navigate = useNavigate();
-const [first, setfirst] = useState(0)
-
-const handleClick = () => {
- SubmitHandler()
-  Tesseract.recognize(dataUrl, "eng", {
-    logger: (m) => {
-      console.log(m);
-      setstatus(m);
-      setfirst(m.progress)
-
-    },
-  }).then(({ data: { text } }) => {
-    setfirst(0)
-    ctx.textHandler(text);
-    console.log(text);
-  });
-
   
-};
-
-
-
-
-if(first===0 && submit ){
-  handleClick();
-}
-
-
 
   return (
     <motion.div
@@ -77,35 +70,49 @@ if(first===0 && submit ){
           {status === 1 ? "completed" : status}%
         </p>
       </div> */}
-      
-      
+
       <div className={classes.navbar}>
-        <div className={classes["backbutton"]} onClick={() => navigate(-1)}> </div>
-        <div className={classes["info"]} onClick={insideHandler}>i</div>
+        <div className={classes["backbutton"]} onClick={() => navigate(-1)}>
+       
+        </div>
+        <p className={`${classes.navNumber} ${classes.mainNavNumber}`}>Saved - 1</p>
+        <p className={classes.navText}>Convert Images to text</p>
+       
       </div>
 
       <div className={classes.getText}>
-        <button onClick={SubmitHandler} >
-          Get text
-        </button>
-      </div>
-      <div className={classes.textDiv}>
+        <button className={classes.getTextButton}onClick={SubmitHandler}>Get text</button>
+        <div className={classes.textDiv}>
+          <div className={classes.textDivNavbar}>
+            <p className={`${classes.navNumber} ${classes.getTextNavNumber}`}>Copy</p>
+          </div>
         <p className={classes.extractedText}>
           {ctx.text ? ctx.text : "Sample Text here"}
         </p>
-        <button onClick={ctx.textlistHandler}>save</button>
+        <div className={classes.convertMore}>
+        <button onClick={ctx.textlistHandler}>Save</button>
+        <button onClick={ctx.textlistHandler}>Add more</button>
+        </div>
+        
       </div>
-   { inside &&
-    <ImageModal dataUrl={dataUrl}status={statusHandler} SubmitHandler={SubmitHandler} submit={submit}/>}
-        {/* <Image
-
-    submit={submit}
-    texte={ctx.textHandler}
-    submitset={SubmitHandler}
-    status={statusHandler}
-  />
-   */}
-      <Canvas preprocess={submit} dataUrl={DataUrlHandler} submit={props.submit} />
+      </div>
+     
+      <div className={classes["imagesModal"]} onClick={insideHandler}>
+         <p></p>
+        </div> 
+<AnimatePresence>
+      {inside && (
+        <ImageModal
+          dataUrl={dataUrl}
+          isOpen={inside}
+        />
+      )}
+     </AnimatePresence>
+      <Canvas
+        preprocess={submit}
+        dataUrl={DataUrlHandler}
+        submit={props.submit}
+      />
     </motion.div>
   );
 };
