9045a2056b9429a6b9f6a3397e6ccd0c090550f5
Dharamveer <workfordharamveer@gmail.com>
2022-06-12 11:17:49 +0530

    j

--- src/components/image.js
+++ src/components/image.js
@@ -1,50 +1,50 @@
-import { useContext, useEffect, useState } from "react";
+// import { useContext, useEffect, useState } from "react";
 
-import AuthContext from "../store/auth-context";
-import classes from "./image.module.css";
+// import AuthContext from "../store/auth-context";
+// import classes from "./image.module.css";
 
-const Image = (props) => {
-  const [first, setfirst] = useState(0)
-  const ctx = useContext(AuthContext);
-  const handleClick = () => {
-    props.submitset();
-    Tesseract.recognize(props.dataUrl, "eng", {
-      logger: (m) => {
-        console.log(m);
-        props.status(m);
-        setfirst(m.progress)
+// const Image = (props) => {
+//   const [first, setfirst] = useState(0)
+//   const ctx = useContext(AuthContext);
+//   const handleClick = () => {
+//     props.submitset();
+//     Tesseract.recognize(props.dataUrl, "eng", {
+//       logger: (m) => {
+//         console.log(m);
+//         props.status(m);
+//         setfirst(m.progress)
 
-      },
-    }).then(({ data: { text } }) => {
-      setfirst(0)
-      props.texte(text);
-      console.log(text);
-    });
+//       },
+//     }).then(({ data: { text } }) => {
+//       setfirst(0)
+//       props.texte(text);
+//       console.log(text);
+//     });
 
     
-  };
+//   };
 
 
   
  
-  if(first===0 && props.submit ){
-    handleClick();
-  }
+//   if(first===0 && props.submit ){
+//     handleClick();
+//   }
  
    
 
-  return (
-    <div className={classes.imageDiv}>
-      <p>Image</p>
-      <div className={classes.img}>
-        <img
-          src={ctx.image && URL.createObjectURL(ctx.image)}
-          className={classes.img}
-        />
-      </div>
+//   return (
+//     <div className={classes.imageDiv}>
+//       <p>Image</p>
+//       <div className={classes.img}>
+//         <img
+//           src={ctx.image && URL.createObjectURL(ctx.image)}
+//           className={classes.img}
+//         />
+//       </div>
       
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default Image;
+// export default Image;
